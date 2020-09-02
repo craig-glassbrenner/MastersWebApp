05a5eeda1aa342bfa06d19f8065287c68138be1c
Craig Glassbrenner <craigyg8@yahoo.com>
2020-09-02 12:11:10 -0500

    Templates

--- setup.py
+++ setup.py
@@ -1,10 +1,10 @@
-from flask import Flask, redirect, url_for, render_template
+from flask import Flask, redirect, url_for, render_template 
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-	return "Hello world! <h1>YO<h1>"
+	return render_template("index.html")
 
 if __name__ == "__main__":
-	app.run()
+	app.run(debug=True)
